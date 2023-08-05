2765ba2ec2d3aa1fe02dc0217e945089ba185def
gohardbro <wkd9155@gmail.com>
2023-08-05 15:10:35 +0900

    html 수정

--- src/main/java/dev/gohard/youtubemanager/controller/HomeController.java
+++ src/main/java/dev/gohard/youtubemanager/controller/HomeController.java
@@ -1,16 +1,13 @@
 package dev.gohard.youtubemanager.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "home!";
+        return "redirect:/login";
     }
-
-    @GetMapping("/secured")
-    public String secured() {return "secured!";}
 }
